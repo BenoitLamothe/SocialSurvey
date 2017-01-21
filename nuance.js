64442895e1f04592bef1d499b8d7080c32d42346
Olivier Boucher <info@olivierboucher.com>
2017-01-21 16:31:35 -0500

    added reddit infos

--- nuance.js
+++ nuance.js
@@ -1,71 +1,20 @@
-const WebSocketClient = require('websocket').w3cwebsocket;
+const rp = require('request-promise-native');
 
-const HOST = 'nim-rd.nuance.mobi';
-const PORT = 9443;
-const SOCKET_PATH = 'nina-webapi/nina';
-
-
-const Nuance = (identity, credentials) => {
-		return new Promise((resolve) => {
-				new NuanceAPI(identity, credentials, resolve)
-		})
-};
-
-class NuanceAPI {
-	constructor(identity, credentials, rdyfn) {
-		this.identity = identity;
-		this.credentials = credentials;
-		this.rdyfn = rdyfn;
-		this.ws = new WebSocketClient(`wss://${HOST}:${PORT}/${SOCKET_PATH}`);
-
-		this.ws.onopen = this._onOpen.bind(this);
-		this.ws.onclose = this._onClose.bind(this);
-		this.ws.onmessage = this._onMessage.bind(this);
-	}
-
-	_onOpen() {
-		console.log('_onOpen', JSON.stringify({
-			connect: this.identity,
-		}));
-		console.log('_onOpen', JSON.stringify({
-			command: Object.assign({
-				name: "NinaStartSession",
-				apiVersion: 'LATEST',
-			}, this.credentials)
-		}));
-		this.ws.send(JSON.stringify({
-			connect: this.identity,
-		}));
-		this.ws.send(JSON.stringify({
-			command: Object.assign({
-				name: "NinaStartSession",
-				apiVersion: 'LATEST',
-			}, this.credentials)
-		}));
-	}
-
-	_onClose(event) {
-		console.log('_onClose', event)
-	}
-
-	_onMessage(event) {
-		console.log('_onMessage', event.data);
-
-		const response = JSON.parse(event.data);
-
-		switch(Object.keys(response)[0]){
-			case 'QueryResult':
-					if (response.QueryResult.result_type === 'NinaStartSession' && response.QueryResult.final_response === true) {
-						this.rdyfn(this);
-					}
-				break;
+module.exports = class Nuance {
+		constructor(credentials, identity) {
+			this._identity = identity;
+			this._credentials = credentials;
 		}
-	}
 
-	send(msg) {
-		console.log('send', JSON.stringify(msg));
-		this.ws.send(JSON.stringify(msg))
-	}
-}
+		getNLU(text, params = {}) {
+			const options = {
+				method: 'POST',
+				uri: 'https://webapi-demo.nuance.mobi:11443/nina-webapi/NinaDoNLU/',
+				headers: Object.assign({}, this._credentials),
+				json: true,
+				body: Object.assign({}, this._identity, params, { text, nlu_engine: 'NLE' })
+			};
 
-module.exports = Nuance;
+			return rp(options);
+		}
+};
