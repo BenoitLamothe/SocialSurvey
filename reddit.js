421432fb7b634a0610710dc5dde1681c16cfd0e1
Olivier Boucher <info@olivierboucher.com>
2017-01-21 21:55:41 -0500

    added provider

--- reddit.js
+++ reddit.js
@@ -23,7 +23,7 @@ class RedditClient {
 				.then(submissions => submissions.filter(x => x.comments.length > 0))
 				.then(submissions => submissions.map(x => x.comments))
 				.then(comments => comments.reduce((a, b) => [...a, ...b], []))
-				.then(comments => comments.map(x => ({ raw_text: x.body })))
+				.then(comments => comments.map(x => ({ raw_text: x.body, provider: 'reddit' })))
 				.catch(() => []);
 	}
 }
