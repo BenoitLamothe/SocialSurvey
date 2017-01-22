6e8bc9e43302c84d71df557a23395253b73da8aa
Jeremie Poisson <jeremie@weboxigen.com>
2017-01-21 19:15:47 -0500

    added sanitize

--- sanitize.js
+++ sanitize.js
@@ -1,7 +1,9 @@
 module.exports = {
     sanitizeText: (str) => {
-        //const twitterRe = //;
-        // filter out twitter hahstags and handles
-        // filter out url
+        const twitterRe = /([@#])([a-z\d_]+)/i;
+        const urlRe = /(https?|ftp):\/\/[^\s/$.?#].[^\s]*/i;
+
+        return str.replace(twitterRe, "")
+                   .replace(urlRe, "");
     }
 };
